f398ebbc7d3a9cdb2c9bd79009f9960aa66c298a
Marlo Pato <Marlo_pato123@hotmail.com>
2024-12-06 16:03:03 +0100

    added files  and changed details

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import HabitsPage from "./pages/HabitsPage";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Testing my branch</h1>
+      <Routes>
+        <Route path="/habits" element={<HabitsPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/HabitsPage.jsx
@@ -0,0 +1,9 @@
+const HabitsPage = () => {
+  return (
+    <>
+      <h1>Hej från habits</h1>
+    </>
+  );
+};
+
+export default HabitsPage;
