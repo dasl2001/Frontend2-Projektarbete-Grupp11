7057f99eb24206f072c459f7a232531f7bf91913
dasl2001 <david.slivo2001@gmail.com>
2024-12-18 13:51:12 +0100

    Added a function in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -20,6 +20,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const navigate = useNavigate();
+
   useEffect(() => {
     localStorage.setItem('userList', JSON.stringify(userList));
   }, [userList]);
@@ -34,7 +35,7 @@ function App() {
 
   function handleLogout() {
     setLoggedInUser(null);
-    setLogoutMessage('Loggas ut.');
+    setLogoutMessage('Du har loggats ut.');
     navigate('/');
   }
 
@@ -42,31 +43,40 @@ function App() {
     <>
       <Navbar loggedInUser={loggedInUser} onLogout={handleLogout} />
       <Routes>
-        <Route
-          path="/" element={<Home logoutMessage={logoutMessage} clearLogoutMessage={() => setLogoutMessage('')} />} />
-        <Route path="/login" element={<Login userList={userList} setLoggedInUser={setLoggedInUser} logoutMessage={logoutMessage} clearLogoutMessage={() => setLogoutMessage('')} />} />
-        <Route path="/register" element={<AddUser userList={userList} setUserList={setUserList} />} />
-        <Route path="/habits" element={<HabitsPage />} />
-        <Route path="/todo/:id" element={<TodoDetails tasks={tasks} setTasks={setTasks} />} />
-        <Route path="/start" element={<Start tasks={tasks} />} />
-        <Route path="/todo" element={<Todo tasks={tasks} setTasks={(updatedTasks) => {
-                setTasks(updatedTasks);
-                if (loggedInUser) {
-                  const updatedUser = {
-                    ...loggedInUser,
-                    tasks: updatedTasks,
-                  };
-                  setLoggedInUser(updatedUser);
-                  setUserList((prevList) =>
-                    prevList.map((user) =>
-                      user.username === updatedUser.username ? updatedUser : user
-                    )
-                  );
-                }
-              }}
+        {/* Endast tillgängligt när användaren inte är inloggad */}
+        {!loggedInUser ? (
+          <>
+            <Route path="/login" element={<Login userList={userList} setLoggedInUser={setLoggedInUser} logoutMessage={logoutMessage} clearLogoutMessage={() => setLogoutMessage('')} />} />
+            <Route path="/register" element={<AddUser userList={userList} setUserList={setUserList} />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Home logoutMessage={logoutMessage} clearLogoutMessage={() => setLogoutMessage('')} />} />
+            <Route path="/start" element={<Start tasks={tasks} />} />
+            <Route path="/habits" element={<HabitsPage />} />
+            <Route path="/todo/:id" element={<TodoDetails tasks={tasks} setTasks={setTasks} />} />
+            <Route path="/todo" element={<Todo tasks={tasks} setTasks={(updatedTasks) => {
+                    setTasks(updatedTasks);
+                    if (loggedInUser) {
+                      const updatedUser = {
+                        ...loggedInUser,
+                        tasks: updatedTasks,
+                      };
+                      setLoggedInUser(updatedUser);
+                      setUserList((prevList) =>
+                        prevList.map((user) =>
+                          user.username === updatedUser.username ? updatedUser : user
+                        )
+                      );
+                    }
+                  }}
+                />
+              }
             />
-          }
-        />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        )}
       </Routes>
     </>
   );
