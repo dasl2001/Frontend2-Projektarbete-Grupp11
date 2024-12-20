116ec82108f8d64fee449505743a28222a7991e8
dasl2001 <david.slivo2001@gmail.com>
2024-12-20 12:05:59 +0100

    fixed some issues with navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ Moduler och komponenter importeras.
 import { Link } from 'react-router-dom';
 import '../css/navbar.css';
 
-
-
 /*
 Navbar är en funktion komponent i React.
 loggedInUser är en prop som indikerar om det finns en användare inloggad.
@@ -27,7 +25,6 @@ Länkarna inkluderar /login, /register, /start, /todo, /events, /habits
 loggedInUser ? ... : ...: är en ternär operator som kontrollerar om loggedInUser är true. 
 Om användaren är inloggad visas en knapp: <button onClick={onLogout}>Logga ut</button>.
 När knappen klickas anropas onLogout för att logga ut användaren.
-Om ingen användare är inloggad visas hem-sidan "/". 
 */
   return (
     <nav className="navbar">
@@ -42,13 +39,7 @@ Om ingen användare är inloggad visas hem-sidan "/".
           <li><Link to="/todo">Todos & Activities</Link></li>
           <li><Link to="/events">Event calendar</Link></li>
           <li><Link to="/habits">Habits</Link></li>
-          {loggedInUser ? (
-            <li>
-              <button onClick={onLogout}>Logga ut</button>
-            </li>
-          ) : (
-            <li><Link to="/"></Link></li>
-          )}
+          <li><button onClick={onLogout}>Logga ut</button> </li>
         </ul>
       </div>
     </nav>
